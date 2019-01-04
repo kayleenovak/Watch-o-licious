9a4268f17bb3f2f0396292f1ce1d9936eca61c5d
Kaylee Bunce <kaybunce@hotmail.com>
2019-01-04 10:42:06 -0700

    Add describe blocks to actions tests

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,39 +1,45 @@
 import { dataFetchSuccess, isLoading, hasErrored } from './index.js'
 
 describe('actions', () => {
-  it('dataFetchSuccess should return an object with a type of FETCH_DATA_SUCCESS and tvShows', () => {
-    const mockTvShows = [{title: 'Cake Boss'}, {title: 'Great British Baking Show'}]
-    const expected = {
-      type: 'FETCH_DATA_SUCCESS',
-      tvShows: mockTvShows
-    }
+  describe('dataFetchSuccess', () => {
+    it('should return an object with a type of FETCH_DATA_SUCCESS and tvShows', () => {
+      const mockTvShows = [{title: 'Cake Boss'}, {title: 'Great British Baking Show'}]
+      const expected = {
+        type: 'FETCH_DATA_SUCCESS',
+        tvShows: mockTvShows
+      }
 
-    const result = dataFetchSuccess(mockTvShows)
+      const result = dataFetchSuccess(mockTvShows)
 
-    expect(result).toEqual(expected)
+      expect(result).toEqual(expected)
+    })
   })
 
-  it('isLoading should return an object with a type of IS_LOADING and a check', () => {
-    const check = false
-    const expected = {
-      type: 'IS_LOADING',
-      check
-    }
+  describe('isLoading', () => {
+    it('isLoading should return an object with a type of IS_LOADING and a check', () => {
+      const check = false
+      const expected = {
+        type: 'IS_LOADING',
+        check
+      }
 
-    const result = isLoading(check)
+      const result = isLoading(check)
 
-    expect(result).toEqual(expected)
+      expect(result).toEqual(expected)
+    })
   })
 
-  it('hasErrored should return an object with a type of HAS_ERRORED and a check', () => {
-    const check = false
-    const expected = {
-      type: 'HAS_ERRORED',
-      check
-    }
+  describe('hasErrored', () => {
+    it('hasErrored should return an object with a type of HAS_ERRORED and a check', () => {
+      const check = false
+      const expected = {
+        type: 'HAS_ERRORED',
+        check
+      }
 
-    const result = hasErrored(check)
+      const result = hasErrored(check)
 
-    expect(result).toEqual(expected)
+      expect(result).toEqual(expected)
+    })
   })
 })
