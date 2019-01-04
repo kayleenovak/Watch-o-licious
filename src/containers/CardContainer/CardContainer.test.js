543ce6c526a0f9ccb28dfad940b64cab54f3ebe9
Kaylee Bunce <kaybunce@hotmail.com>
2019-01-04 12:34:24 -0700

    Add tests for CardContainer - render and mapStateToProps

---
+++ src/containers/CardContainer/CardContainer.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { isLoading, hasErrored} from '../../actions/index.js'
+import { CardContainer, mapStateToProps} from './CardContainer.js'
+import { mockCleanCakeShows } from '../../mockCleanData.js'
+import TvShowCard from '../TvShowCard/TvShowCard.js'
+
+describe('CardContainer', () => {
+  it('should render two TvShowCards', () => {
+    const wrapper = shallow(<CardContainer tvShows={ mockCleanCakeShows } />)
+
+    expect(wrapper.find(TvShowCard).length).toBe(2)
+  })
+
+  describe('mapStateToProps', () => {
+    const mockState = {
+      tvShows: mockCleanCakeShows,
+      isLoading,
+      hasErrored
+    }
+    const expected = {
+      tvShows: mockCleanCakeShows
+    }
+
+    const result = mapStateToProps(mockState)
+
+    expect(result).toEqual(expected)
+  })
+})
