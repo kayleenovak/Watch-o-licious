1973fc1592332a189262aef4446818b136a7afc7
Kaylee Bunce <kaybunce@hotmail.com>
2019-01-04 12:20:34 -0700

    Add tests for App - componentDidMount and mapDispatchToProps

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,4 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App, mapDispatchToProps } from './App';
+import { shallow } from 'enzyme'
+import { fetchTvShows } from '../../thunks/fetchTvShows.js'
 
+describe('App', () => {
+  it('should fire fetchTvShows on componentDidMount', () => {
+    const mockFetchTvShows = jest.fn()
+    const wrapper = shallow(<App  fetchTvShows={ mockFetchTvShows } />)
+
+    wrapper.instance().componentDidMount()
+
+    expect(mockFetchTvShows).toHaveBeenCalled()
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with the correct params', () => {
+      const mockDispatch = jest.fn()
+      const mockUrl = 'www.thisurl.com'
+      const mappedProps = mapDispatchToProps(mockDispatch)
+
+      mappedProps.fetchTvShows(mockUrl)
+
+      expect(mockDispatch).toHaveBeenCalledWith(expect.any(Function))
+    })
+  })
+})
