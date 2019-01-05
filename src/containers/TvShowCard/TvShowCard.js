7f90eff5d9461b33135f7ad258945cc2db9b40d4
Kaylee Bunce <kaybunce@hotmail.com>
2019-01-05 11:27:00 -0700

    Add styling to tvShowCards

--- src/containers/TvShowCard/TvShowCard.js
+++ src/containers/TvShowCard/TvShowCard.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import './TvShowCard.css'
 
 export default class TvShowCard extends Component {
   render() {
-    const { title, network, rating, image} = this.props
+    const { title, network, image} = this.props
     return (
-      <article>
-      <img src={image} />
-        <h3>{title}</h3>
-        <p>Network: {network}</p>
-        <p>{rating}</p>
+      <article className='tv-show-card'>
+      <img src={image} className='tv-card-img'/>
+        <h3 className='tv-card-title'>{title}</h3>
+        <p className='tv-card-network'>Network: {network}</p>
       </article>
     )
   }
