f132fc5cb7b35f95745babcf35734e3881a57995
Kaylee Bunce <kaybunce@hotmail.com>
2019-01-05 12:59:30 -0700

    Add mapDsipatchToProps to TvShowCard

--- src/containers/TvShowCard/TvShowCard.js
+++ src/containers/TvShowCard/TvShowCard.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { fetchTvShowInfo } from '../../thunks/fetchTvShowInfo.js'
 import './TvShowCard.css'
 
 export default class TvShowCard extends Component {
   render() {
     const { title, network, image } = this.props
     return (
-      <article className='tv-show-card' >
+      <article className='tv-show-card' onClick={this.props.fetchShowInfo(this.props.id)}>
         <img src={image} className='tv-card-img'/>
         <h3 className='tv-card-title'>{title}</h3>
         <p className='tv-card-network'>Network: {network}</p>
@@ -14,4 +15,10 @@ export default class TvShowCard extends Component {
     )
   }
 }
+
+export const mapDispatchToProps = (dispatch) => ({
+  fetchShowInfo: (id) => dispatch(fetchTvShowInfo(id))
+})
+
+export default connect(null, mapDispatchToProps)(TvShowCard)
 
