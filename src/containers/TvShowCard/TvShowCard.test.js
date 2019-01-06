70f07519df94e5b9bc02c0bbc6165d757ae0a41a
Kaylee Bunce <kaybunce@hotmail.com>
2019-01-05 23:22:06 -0700

    Adjust test in TvShowCard

--- src/containers/TvShowCard/TvShowCard.test.js
+++ src/containers/TvShowCard/TvShowCard.test.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import TvShowCard from './TvShowCard.js'
+import { TvShowCard } from './TvShowCard.js'
 import { mockCleanCakeShows } from '../../mockCleanData.js'
 import { shallow } from 'enzyme'
 
 describe('TvShowCard', () => {
   it('should return an article with an imaage, h3, 2 p elements', () => {
-    const wrapper = shallow(<TvShowCard {...mockCleanCakeShows[0]} />)
+    const wrapper = shallow(<TvShowCard title={'Cake Boss'} />)
 
     expect(wrapper.find('article').length).toEqual(1)
     expect(wrapper.find('h3').length).toEqual(1)
     expect(wrapper.find('img').length).toEqual(1)
-    expect(wrapper.find('p').length).toEqual(2)
+    expect(wrapper.find('p').length).toEqual(1)
   })
 })
