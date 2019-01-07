27bda1c8b444fd1ed030c3516e0fcef923912bd7
Kaylee Bunce <kaybunce@hotmail.com>
2019-01-07 10:12:50 -0700

    Hook up buttons in favoriteCard to actions to toggle tracked properties

--- src/containers/FavoriteCard/FavoriteCard.js
+++ src/containers/FavoriteCard/FavoriteCard.js
@@ -3,25 +3,59 @@ import { connect } from 'react-redux'
 import { addToTracked, removeFromTracked, toggleFavorite, toggleWatched, toggleWatchList } from '../../actions/index.js'
 
 export class FavoriteCard extends Component {
+    
+  handleTrackedEpisode = (e) => {
+    console.log(1)
+    const matchedEpisode = this.props.tracked.find(episode => {
+      return this.props.episode.url === episode.url
+    })
+    if(matchedEpisode === undefined) {
+      this.props.addToTracked(this.props.episode)
+      this.handleToggleTracked(e)
+    } else {
+      this.handleToggleTracked(e)
+    }
+  }
+
+  handleToggleTracked = (e) => {
+    if (e.target.value === 'favorite') {
+      this.props.toggleFavorite(this.props.episode)
+    } else if (e.target.value === 'watched') {
+      this.props.toggleWatched(this.props.episode)
+    } else if (e.target.value === 'watchlist') {
+      this.props.toggleWatchList(this.props.episode)
+    }
+    this.handleRemoveTracked()
+  }
+
+  handleRemoveTracked = () => {
+    const { favorite, watchlist, watched } = this.props.episode.tracked
+    if (favorite === false && watchlist === false && watched === false) {
+      this.props.removeFromTracked(this.props.episode)
+    }
+  }
+
   render() {
     const matchedTvShow = this.props.tvShows.find(show => {
       return show.id === parseInt(this.props.episode.showId)
     })
+
     return (
       <section>
         <img src={matchedTvShow.image} />
         <h3>Episode: {this.props.episode.title}</h3>
         <h5>{matchedTvShow.title}</h5>
-        <button>Favorite</button>
-        <button>Watch List</button>
-        <button>Watched</button>
+        <button onClick={(e) => this.handleTrackedEpisode(e)} value='favorite'>Favorite</button>
+        <button onClick={(e) => this.handleTrackedEpisode(e)} value='watchlist'>Watch List</button>
+        <button onClick={(e) => this.handleTrackedEpisode(e)} value='watched'>Watched</button>
       </section>
     )
   }
 }
 
 export const mapStateToProps = (state) => ({
-  tvShows: state.tvShows
+  tvShows: state.tvShows,
+  tracked: state.tracked
 })
 
 export const mapDispatchToProps = (dispatch) => ({
