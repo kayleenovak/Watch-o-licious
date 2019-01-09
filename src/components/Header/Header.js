4ece1edb7cb63cf6d8d3f844031a651da3ea56d0
Kaylee Bunce <kaybunce@hotmail.com>
2019-01-08 17:53:22 -0700

    Add styling to header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
+import './Header.css'
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <NavLink to='/favorites'><button>Favorites</button></NavLink>
-        <NavLink to='/watched'><button>Watched</button></NavLink>
-        <NavLink to='/watchlist'><button>Watch List</button></NavLink>
-        <NavLink to='/'><button>Home</button></NavLink>
+      <div className='header'>
+        <img src='/cupcake.svg' className='cupcake' />
+        <h1 className='heading'>Watch-o-licious</h1>
+        <section className='buttons'>
+          <NavLink to='/favorites'><button className='header-btn'>Favorites</button></NavLink>
+          <NavLink to='/watched'><button className='header-btn'>Watched</button></NavLink>
+          <NavLink to='/watchlist'><button className='header-btn'>Watch List</button></NavLink>
+          <NavLink to='/'><button className='header-btn'>Home</button></NavLink>
+        </section>
       </div>
     )
   }
