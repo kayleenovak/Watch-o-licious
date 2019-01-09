8ce2c0209f51bf96df6ba965e26a5a91dd8bd079
Kaylee Bunce <kaybunce@hotmail.com>
2019-01-09 02:06:57 -0700

    Add tests to MainPage container

--- src/containers/MainPage/MainPage.test.js
+++ src/containers/MainPage/MainPage.test.js
@@ -4,8 +4,17 @@ import { MainPage, mapStateToProps } from './MainPage.js'
 import { FeaturedTvShow } from '../FeaturedTvShow/FeaturedTvShow.js'
 import CardContainer from '../CardContainer/CardContainer.js'
 import { mockCleanCakeShows } from '../../mockCleanData.js'
+import { Loading } from '../../components/Loading/Loading.js'
 
 describe('MainPage', () => {
+  it('should match the snapshot', () => {
+    const mockError = false
+    const mockLoading = false
+    const wrapper = shallow(<MainPage tvShows={ mockCleanCakeShows } isLoading={ mockLoading } hasErrored={ mockError }/>)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
   it('should render a FeaturedTvShow and CardContainer component if there are tvShows', () => {
     const wrapper = shallow(<MainPage tvShows={ mockCleanCakeShows } />)
 
@@ -19,6 +28,21 @@ describe('MainPage', () => {
     expect(wrapper.find(FeaturedTvShow).length).toEqual(0)
     expect(wrapper.find(CardContainer).length).toEqual(0)
   })
+
+  it('should render a Loading component if isLoading is true', () => {
+    const mockLoading = true
+    const wrapper = shallow(<MainPage isLoading={mockLoading} />)
+
+    expect(wrapper.find(Loading).length).toEqual(1)
+  })
+
+  it('should render a div component if hasErrored is true', () => {
+    const mockErrored = true
+    const wrapper = shallow(<MainPage hasErrored={mockErrored} />)
+
+    expect(wrapper.find('div').length).toEqual(1)
+  })
+
 
   describe('mapStateToProps', () => {
     it('should return an object with tvShows as a key', () => {
