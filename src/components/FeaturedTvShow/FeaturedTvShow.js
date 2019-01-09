825532cdc3c7de4db0c7e058de13e5ab96be181c
Kaylee Bunce <kaybunce@hotmail.com>
2019-01-09 03:09:43 -0700

    Add proptypes to FeaturedTvShow

--- src/components/FeaturedTvShow/FeaturedTvShow.js
+++ src/components/FeaturedTvShow/FeaturedTvShow.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './FeaturedTvShow.css'
+import PropTypes from 'prop-types'
 
 export const FeaturedTvShow = (props) => {
     const {title, runtime, premiered, rating, summary, image, id} = props
@@ -19,5 +20,16 @@ export const FeaturedTvShow = (props) => {
         <img alt='series-poster' className='featured-img' src={image} />
       </section>
     )
+}
+
+FeaturedTvShow.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  runtime: PropTypes.number.isRequired,
+  premiered: PropTypes.string.isRequired,
+  schedule: PropTypes.object,
+  network: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired
 }
 
